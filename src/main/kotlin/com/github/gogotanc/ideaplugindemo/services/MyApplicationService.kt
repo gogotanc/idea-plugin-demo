2dd3c8ed80ba2bd4dd61985053b400b51c4577af
GitHub Action <action@github.com>
2021-11-02 03:35:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/gogotanc/ideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gogotanc/ideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gogotanc.ideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gogotanc.ideaplugindemo.MyBundle
 
 class MyApplicationService {
 
